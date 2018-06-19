a2676f8df4c44d9ee74ec12490d4089799af2da1
sugunasagar <sugunasagar.sg@allegion.com>
2018-06-19 15:17:34 +0530

    Testing GIT Commit

--- Test_Sample/Deviation/Deviation/WT_HOME/src/com/irco/deviation/DeviationResource.java
+++ Test_Sample/Deviation/Deviation/WT_HOME/src/com/irco/deviation/DeviationResource.java
@@ -11,7 +11,7 @@ public class DeviationResource extends ListResourceBundle
 {
 
 
-	//Changes done by SUguna Sagar 
+	//Changes done by SUguna Sagar 1
 
     static final Object contents[][] = {
        {
